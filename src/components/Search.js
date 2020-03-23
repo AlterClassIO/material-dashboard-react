e33547dcf6dcd1978d591163a1e4c768c9dc59df
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-03-23 20:40:37 +0100

    styles: change search container default width

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,11 @@
 import React, { useState, useRef } from "react";
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+// custom hooks
+import { useEventListener } from '../hooks';
 // @material-ui/core components
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
-// hooks
-import { useEventListener } from '../hooks';
 
 // styled components
 const Container = styled.div`
@@ -14,19 +14,17 @@ const Container = styled.div`
   align-items: center;
   background-color: rgba(255, 255, 255, 0.15);
   border-radius: 5px;
-  width: ${props => props.wider ? '250px' : '200px'};
+  width: ${props => props.wider ? '250px' : '180px'};
   transition: width 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
 
   &:hover {
     background-color: rgba(255, 255, 255, 0.25);
   }
 `;
-
 const SearchIconContainer = styled(SearchIcon)`
   position: absolute;
   margin: 0 16px;
 `;
-
 const StyledInput = styled(InputBase)`
   color: #fff;
   padding: 4px 8px;
@@ -36,9 +34,11 @@ const StyledInput = styled(InputBase)`
 
 // main component
 const Search = ({ placeholder = 'Search…' }) => {
+  // state
   const [wider, setWider] = useState(false);
+  // ref to help detect click inside/outside container
   const containerEl = useRef(null);
-
+  // function to toggle search bar width
   const handleOnClick = event => {
     if (containerEl.current && !containerEl.current.contains(event.target)) {
       setWider(false);
@@ -47,9 +47,9 @@ const Search = ({ placeholder = 'Search…' }) => {
       setWider(!wider);
     }
   };
-
+  // use hook to detect click event
   useEventListener('mousedown', handleOnClick);
-
+  // render component
   return (
     <Container 
       ref={containerEl}
