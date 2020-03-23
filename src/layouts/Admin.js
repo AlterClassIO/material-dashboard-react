985511dec9b8731a164a63e7e799890cfa9efbfd
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-03-23 16:57:16 +0100

    feat: create admin layout

---
+++ src/layouts/Admin.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+// @material-ui/core components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+// core components
+//...
+
+const drawerWidth = 240;
+const drawerHeight = 64;
+
+// styled components
+const Layout = styled.div`
+  display: flex;
+`;
+
+const StyledAppBar = styled(AppBar)`
+  margin-left: ${props => props.shifted ? `${drawerWidth}px` : '0'};
+  width: ${props => props.shifted ? `calc(100% - ${drawerWidth}px)` : '100%'};
+`;
+
+const StyledIconButton = styled(IconButton)`
+  display: ${props => props.show ? 'inline-block' : 'none'};
+`;
+
+const StyledDrawer = styled(Drawer)`
+  width: ${drawerWidth}px;
+  flex-shrink: 0;
+`;
+
+const DrawerContent = styled.div`
+  width: ${drawerWidth}px;
+`;
+
+const DrawerHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0 8px;
+  height: ${drawerHeight}px;
+`;
+
+const Content = styled.main`
+  flex-grow: 1;
+  margin-left: ${props => props.shifted ? '0' : `-${drawerWidth}px`};
+`;
+
+// main component
+const AdminLayout = ({ children }) => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  return (
+    <Layout>
+      { /* HEADER BAR */ }
+      <StyledAppBar position="fixed" shifted={openDrawer ? 1 : 0}>
+        <Toolbar>
+          <StyledIconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            show={openDrawer ? 0 : 1}
+          >
+            <MenuIcon />
+          </StyledIconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </StyledAppBar>
+
+      { /* DRAWER MENU */ }
+      <StyledDrawer
+        variant="persistent"
+        anchor="left"
+        open={openDrawer}
+      >
+        <DrawerContent>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+        </DrawerContent>
+      </StyledDrawer>
+
+      { /* MAIN CONTENT */ }
+      <Content shifted={openDrawer ? 1 : 0}>
+        {children}
+      </Content>
+    </Layout>
+  );
+};
+
+export default AdminLayout;
