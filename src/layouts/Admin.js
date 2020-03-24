46e40cab2de53af55975b7c7229bfef735fba574
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-03-24 19:09:25 +0100

    refact: render part of components from distinct functions

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -14,6 +14,7 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
 import HomeIcon from '@material-ui/icons/Home';
 import SettingsIcon from '@material-ui/icons/Settings';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
@@ -21,6 +22,8 @@ import ListIcon from '@material-ui/icons/List';
 import AppsIcon from '@material-ui/icons/Apps';
 import CodeIcon from '@material-ui/icons/Code';
 import BarChartIcon from '@material-ui/icons/BarChart';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 // core components
 import Search from '../components/Search';
 
@@ -60,6 +63,19 @@ const DrawerHeader = styled.div`
 const Content = styled.main`
   flex-grow: 1;
   margin-left: ${props => props.shifted ? '0' : `-${drawerWidth}px`};
+  margin-top: ${drawerHeight}px;
+  padding: 24px;
+`;
+const NestedListItem = styled(ListItem)`
+  padding-left: 56px;
+`;
+const Dot = styled.span`
+  width: 4px;
+  height: 4px;
+  border-radius: 100%;
+  background-color: rgba(0, 0, 0, 0.87);
+  display: inline-block;
+  margin-right: 15px;
 `;
 
 // main component
@@ -71,6 +87,7 @@ const AdminLayout = (props) => {
   } = props;
   // state
   const [openDrawer, setOpenDrawer] = useState(false);
+  const [openSettings, setOpenSettings] = useState(false);
   // functions to handle drawer opening/closing
   const handleDrawerOpen = () => {
     setOpenDrawer(true);
@@ -78,88 +95,128 @@ const AdminLayout = (props) => {
   const handleDrawerClose = () => {
     setOpenDrawer(false);
   };
+  // functions to handle nested lists opening/closing
+  const handleClickSettings = () => {
+    setOpenSettings(!openSettings);
+  };
+
   // render component
-  return (
-    <Layout>
-      { /* HEADER BAR */ }
-      <StyledAppBar 
-        position="fixed" 
-        shifted={openDrawer ? 1 : 0}
-      >
-        <Toolbar>
-          <StyledIconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            show={openDrawer ? 0 : 1}
-          >
-            <MenuIcon />
-          </StyledIconButton>
-          <Typography variant="h6" noWrap>
-            {title}
-          </Typography>
-          <ToolbarContent>
-            <Search />
-          </ToolbarContent>
-        </Toolbar>
-      </StyledAppBar>
-      { /* DRAWER MENU */ }
-      <StyledDrawer
+  const renderAppBar = () => (
+    <StyledAppBar 
+      position="fixed" 
+      shifted={openDrawer ? 1 : 0}
+    >
+      <Toolbar>
+        <StyledIconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          show={openDrawer ? 0 : 1}
+        >
+          <MenuIcon />
+        </StyledIconButton>
+        <Typography variant="h6" noWrap>
+          {title}
+        </Typography>
+        <ToolbarContent>
+          <Search />
+        </ToolbarContent>
+      </Toolbar>
+    </StyledAppBar>
+  );
+
+  const renderDrawer = () => (
+    <StyledDrawer
         variant="persistent"
         anchor="left"
         open={openDrawer}
       >
-        <DrawerContent>
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </DrawerHeader>
-          <List
-            subheader={<ListSubheader>Pages</ListSubheader>}
-          >
-            <ListItem button>
-              <ListItemIcon><HomeIcon /></ListItemIcon>
-              <ListItemText primary='Dashboard' />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon><BarChartIcon /></ListItemIcon>
-              <ListItemText primary='Management' />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon><SettingsIcon /></ListItemIcon>
-              <ListItemText primary='Settings' />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon><LockOpenIcon /></ListItemIcon>
-              <ListItemText primary='Authentication' />
-            </ListItem>
-          </List>
-          <List
-            subheader={
-              <ListSubheader>Support</ListSubheader>
-            }
-          >
-            <ListItem button>
-              <ListItemIcon><AppsIcon /></ListItemIcon>
-              <ListItemText primary='Components' />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon><CodeIcon /></ListItemIcon>
-              <ListItemText primary='Getting started' />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon><ListIcon /></ListItemIcon>
-              <ListItemText primary='Changelog' />
-            </ListItem>
-          </List>
-        </DrawerContent>
-      </StyledDrawer>
+      <DrawerContent>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+        <List
+          dense={true}
+          subheader={<ListSubheader>Pages</ListSubheader>}
+        >
+          <ListItem button>
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary='Dashboard' />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><BarChartIcon /></ListItemIcon>
+            <ListItemText primary='Management' />
+          </ListItem>
+          <ListItem button onClick={handleClickSettings}>
+            <ListItemIcon><SettingsIcon /></ListItemIcon>
+            <ListItemText primary='Settings' />
+            {openSettings ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={openSettings} timeout="auto" unmountOnExit>
+            <List dense={true}>
+              <NestedListItem button>
+                <Dot />
+                <ListItemText primary='General' />
+              </NestedListItem>
+              <NestedListItem button>
+                <Dot />
+                <ListItemText primary='Subscription' />
+              </NestedListItem>
+              <NestedListItem button>
+                <Dot />
+                <ListItemText primary='Notifications' />
+              </NestedListItem>
+              <NestedListItem button>
+                <Dot />
+                <ListItemText primary='Security' />
+              </NestedListItem>
+            </List>
+          </Collapse>
+          <ListItem button>
+            <ListItemIcon><LockOpenIcon /></ListItemIcon>
+            <ListItemText primary='Authentication' />
+          </ListItem>
+        </List>
+        <List
+          dense={true}
+          subheader={
+            <ListSubheader>Support</ListSubheader>
+          }
+        >
+          <ListItem button>
+            <ListItemIcon><AppsIcon /></ListItemIcon>
+            <ListItemText primary='Components' />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><CodeIcon /></ListItemIcon>
+            <ListItemText primary='Getting started' />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><ListIcon /></ListItemIcon>
+            <ListItemText primary='Changelog' />
+          </ListItem>
+        </List>
+      </DrawerContent>
+    </StyledDrawer>
+  );
+
+  const renderContent = () => (
+    <Content shifted={openDrawer ? 1 : 0}>
+      {children}
+    </Content>
+  );
+
+  return (
+    <Layout>
+      { /* HEADER BAR */ }
+      {renderAppBar()}
+      { /* DRAWER MENU */ }
+      {renderDrawer()}
       { /* MAIN CONTENT */ }
-      <Content shifted={openDrawer ? 1 : 0}>
-        {children}
-      </Content>
+      {renderContent()}
     </Layout>
   );
 };
