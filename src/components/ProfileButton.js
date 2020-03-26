72ecf234a427f9390ea0c893b7f6d1b091f9aba8
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-03-26 11:19:25 +0100

    feat: create ProfileButton component

---
+++ src/components/ProfileButton.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+// @material-ui/core components
+import Button from '@material-ui/core/Button';
+// core components
+import AvatarWithBadge from "./AvatarWithBadge";
+
+// styled components
+const StyledButton = styled(Button)`
+  .MuiButton-label {
+    text-transform: capitalize;
+    color: #fff;
+  }
+`;
+
+// main component
+const ProfileButton = (props) => {
+  const {
+    text = "Hey there!",
+    src = "",
+    alt = ""
+  } = props;
+  // render component
+  return (
+    <StyledButton
+      endIcon={
+        <AvatarWithBadge alt={alt} src={src} />
+      }
+    >
+      {text}
+    </StyledButton>
+  );
+};
+
+ProfileButton.propTypes = {
+  text: PropTypes.string,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+};
+
+export default ProfileButton;
