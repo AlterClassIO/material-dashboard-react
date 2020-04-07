99fa482a6516a7f1b7ce026fb99782376d9a1b9d
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-04-07 17:05:32 +0200

    feat: add pagination to table

--- src/components/EnhancedTable.js
+++ src/components/EnhancedTable.js
@@ -10,6 +10,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -65,6 +66,16 @@ const EnhancedTable = (props) => {
   // state
   const [selected, setSelected] = useState([]);
   const [anchorMenu, setAnchorMenu] = useState(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  // methos to handle pagination
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   // methods to handle items selection
   const handleSelectItem = (id) => {
     const selectedIndex = selected.indexOf(id);
@@ -122,54 +133,65 @@ const EnhancedTable = (props) => {
       </StyledToolbar>
       <TableContainer>
         <Table>
-        <StyledTableHead>
-          <TableRow>
-            <TableCell padding="checkbox">
-              <Checkbox
-                indeterminate={selected.length > 0 && selected.length < data.length}
-                checked={data.length > 0 && selected.length === data.length}
-                onChange={handleSelectAll}
-              />
-            </TableCell>
-            {header.map(item => <TableCell key={item}>{item}</TableCell>)}
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </StyledTableHead>
-        <TableBody>
-          {
-            data.map(row => (
-              <TableRow
-                key={row.id}
-                hover
-                role="checkbox"
-              >
-                <TableCell 
-                  padding="checkbox"
-                  onClick={e => handleSelectItem(row.id)}
-                >
-                  <Checkbox checked={isSelected(row.id)} />
-                </TableCell>
-                {
-                  Object.entries(row).map((column, id) => {
-                    if (excludeColumns.indexOf(column[0]) !== -1) {
-                      return null;
+          <StyledTableHead>
+            <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  indeterminate={selected.length > 0 && selected.length < data.length}
+                  checked={data.length > 0 && selected.length === data.length}
+                  onChange={handleSelectAll}
+                />
+              </TableCell>
+              {header.map(item => <TableCell key={item}>{item}</TableCell>)}
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </StyledTableHead>
+          <TableBody>
+            {
+              data
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(row => (
+                  <TableRow
+                    key={row.id}
+                    hover
+                    role="checkbox"
+                  >
+                    <TableCell 
+                      padding="checkbox"
+                      onClick={e => handleSelectItem(row.id)}
+                    >
+                      <Checkbox checked={isSelected(row.id)} />
+                    </TableCell>
+                    {
+                      Object.entries(row).map((column, id) => {
+                        if (excludeColumns.indexOf(column[0]) !== -1) {
+                          return null;
+                        }
+                        return (
+                          <TableCell key={id}>{column[1]}</TableCell>
+                        );
+                      })
                     }
-                    return (
-                      <TableCell key={id}>{column[1]}</TableCell>
-                    );
-                  })
-                }
-                <TableCell>
-                  <Button variant="outlined" color="primary">
-                    View
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))
-          }
-        </TableBody>
-      </Table>
+                    <TableCell>
+                      <Button variant="outlined" color="primary">
+                        View
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </Container>
   );
 };
