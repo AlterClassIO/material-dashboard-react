71ab8e340f37431b12f6dbb0a50d19187c178b49
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-04-14 20:00:44 +0200

    feat: update dashboard top cards

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,8 +5,11 @@ import { indigo } from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import DollarIcon from '@material-ui/icons/AttachMoney';
 import Avatar from '@material-ui/core/Avatar';
+// @material-ui/icons components
+import DollarIcon from '@material-ui/icons/AttachMoney';
+import FolderIcon from '@material-ui/icons/Folder';
+import PaymentIcon from '@material-ui/icons/Payment';
 // core components
 //...
 
@@ -29,6 +32,22 @@ const StyledAvatar = styled(Avatar)`
   height: 48px;
   background-color: ${indigo[500]};
 `;
+const PercentageUp = styled.span`
+  color: #4caf50;
+  background-color: rgba(76, 175, 80, 0.08);
+  padding: 4px 8px;
+  border-radius: 4px;
+  font-size: 0.75rem;
+  font-weight: 600;
+`;
+const PercentageDown = styled.span`
+  color: #f44336;
+  background-color: rgba(244, 67, 54, 0.08);
+  padding: 4px 8px;
+  border-radius: 4px;
+  font-size: 0.75rem;
+  font-weight: 600;
+`;
 
 // main component
 const Dashboard = () => {
@@ -42,10 +61,30 @@ const Dashboard = () => {
   );
   const renderTopCards = () => {
     const items = [
-      { title: "Today's earnings", text: "$2,400" },
-      { title: "Today's earnings", text: "$2,400" },
-      { title: "Today's earnings", text: "$2,400" },
-      { title: "Today's earnings", text: "$2,400" },
+      { 
+        title: "Today's earnings",
+        text: "$2,400",
+        icon: <DollarIcon />,
+        percentage: 7,
+      },
+      { 
+        title: "Today's orders",
+        text: "153",
+        icon: <PaymentIcon />,
+        percentage: 13.5,
+      },
+      { 
+        title: "ROI per customer",
+        text: "$27.5",
+        icon: <DollarIcon />,
+        percentage: 1,
+      },
+      { 
+        title: "New projects",
+        text: "15",
+        icon: <FolderIcon />,
+        percentage: -11,
+      }
     ];
     return (
       <TopCards container spacing={3}>
@@ -57,15 +96,39 @@ const Dashboard = () => {
                   container
                   justify="space-between"
                   alignItems="center"
+                  spacing={3}
                 >
-                  <Grid item xs={8} key={id}>
+                  <Grid item xs={9}>
                     <Typography variant="overline" component="h3" gutterBottom={true}>{item.title}</Typography>
-                    <Typography variant="h5" component="h3">{item.text}</Typography>
+                    <Grid
+                      container
+                      alignItems="center"
+                      spacing={1}
+                    >
+                      <Grid item>
+                        <Typography variant="h5" component="h3">{item.text}</Typography>
+                      </Grid>
+                      {
+                        item.percentage && (
+                          <Grid item>
+                            {
+                              item.percentage > 0 ?
+                                <PercentageUp>+{item.percentage}%</PercentageUp> :
+                                <PercentageDown>{item.percentage}%</PercentageDown>
+                            }
+                          </Grid>
+                        )
+                      }
+                    </Grid>
                   </Grid>
-                  <AvatarContainer item xs={4} key={id}>
-                    <StyledAvatar>
-                      <DollarIcon />
-                    </StyledAvatar>
+                  <AvatarContainer item xs={3}>
+                    {
+                      item.icon && (
+                        <StyledAvatar>
+                          {item.icon}
+                        </StyledAvatar>
+                      )
+                    }
                   </AvatarContainer>
                 </Grid>
               </StyledCard>
