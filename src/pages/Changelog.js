55e3fa1dec4af7a273a8cff9385deffb5d20dbfd
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-04-27 16:20:13 +0200

    feat: minor changes

--- src/pages/Changelog.js
+++ src/pages/Changelog.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 // @material-ui/core components
 import Typography from '@material-ui/core/Typography';
-// core components
-// ...
-
-// styled components
-// ...
 
 // main component
 const Changelog = () => {
